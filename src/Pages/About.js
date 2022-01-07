1fdbac63517dedb2f24db9257f92b46f6604d176
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2022-01-07 16:43:27 +0100

    Profil

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import '../styles/About.css';
+import pic from "../assets/photo_me.jpg";
 
 function About() {
     return (
-        <div>
-            À propos de moi !
+        <div className='about__container'>
+            <img src={pic} alt="me" className='about__pic' />
+            <p className="about__title">Antoine Jonville, 33ans, Cannes(06, Alpes-Maritimes)</p>
+            <p className="about_desc">J'ai commencé le développement web en autodidacte, étant passionné par le monde de l'informatique,
+            j'ai effectué une formation de 6 mois avec OpenClassrooms sur le parcours Développeur Web. Je suis quelqu'un de curieux et passionné,
+            j'aime expérimenter, découvrir et apprendre au fur et à mesure de mes projets professionnels et personnels.</p>
         </div>
     )
 }
