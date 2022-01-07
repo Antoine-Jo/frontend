3d6002c712980dd6426f2f41ea9943d54fa57948
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2022-01-07 17:39:41 +0100

    Ajout WIP Portfolio

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -10,6 +10,9 @@ function About() {
             <p className="about_desc">J'ai commencé le développement web en autodidacte, étant passionné par le monde de l'informatique,
             j'ai effectué une formation de 6 mois avec OpenClassrooms sur le parcours Développeur Web. Je suis quelqu'un de curieux et passionné,
             j'aime expérimenter, découvrir et apprendre au fur et à mesure de mes projets professionnels et personnels.</p>
+            <div className="about__skills">
+                <h2 className="skills__title">Mes compétences :</h2>
+            </div>
         </div>
     )
 }
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from 'react';
+import '../styles/Portfolio.css';
+import pic from "../assets/404.webp"
 
 function Portfolio() {
     return (
-        <div>
-            <h1>Portfolio</h1>
+        <div className='portfolio__container'>
+            <h1 className='portfolio__title'>WORK IN PROGRESS</h1>
+            <img src={pic} alt="404" className='portfolio__pic' />
         </div>
     )
 }
