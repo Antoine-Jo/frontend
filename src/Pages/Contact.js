2a05e6bc79672f1e7fb87d9c4e392d757f7096e3
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2022-01-08 16:29:55 +0100

    Mise en place formulaire de contact via service d'email

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import FormContact from '../components/FormContact';
 import '../styles/Contact.css';
 
 function Contact() {
     return (
-        <div>
-            <h1></h1>
+        <div className='contact__container'>
+            <h3>Contactez-moi</h3>
+            <p>Via :</p>
+            <img src="https://img.icons8.com/color/48/000000/linkedin-2--v1.png" width='40px' height='40px' />
+            <p>Téléphone : 06.29.89.42.74</p>
+            <p>Email: antoinejonville@yahoo.fr</p>
+            <FormContact/>
         </div>
     )
 }
