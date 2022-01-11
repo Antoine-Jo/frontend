4edad7a7cfae3b40cb6d6beb08a986413c4c3006
Antoine-Jo <83338682+Antoine-Jo@users.noreply.github.com>
2022-01-11 15:55:21 +0100

    Fix bug menu

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -5,12 +5,22 @@ import { Link } from 'react-router-dom';
 
 
 function Home() {
+    
+    const deleteCursor = () => {
+        const cursor = document.querySelector(".Typewriter__cursor")
+        cursor.style.display = "none"
+
+    }
+
     return (
         <div className='home'>
            <h1 className="name">Antoine Jonville</h1> 
            <Typewriter
             onInit={(typewriter) => {
             typewriter.typeString('Développeur Web Junior')
+            .callFunction(() => {
+                deleteCursor()
+              })
             .start();
             }}
             />
